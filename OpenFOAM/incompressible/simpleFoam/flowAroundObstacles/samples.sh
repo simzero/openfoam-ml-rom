19049849679aebdfa7fa7d71ffdaa80cc8c3fc3f
carpemonf <carpemonf@gmail.com>
2023-04-14 13:30:00 +0200

    Cleaning flowAroundObstacles samples script

--- OpenFOAM/incompressible/simpleFoam/flowAroundObstacles/samples.sh
+++ OpenFOAM/incompressible/simpleFoam/flowAroundObstacles/samples.sh
@@ -1,47 +1,4 @@
 #/bin/bash
-#
-# Copyright (c) 2022 Carlos Peña-Monferrer. All rights reserved.
-# This work is licensed under the terms of the GNU LGPL v3.0 license.
-# For a copy, see <https://opensource.org/licenses/LGPL-3.0>.
-#
-
-#run_case() {
-#  PARENT_DIR="runs/$k"
-#  CONSTANT_DIR="$PARENT_DIR/constant"
-
-#  mkdir -p "$CONSTANT_DIR" "$PARENT_DIR/system" "$PARENT_DIR/0.orig"
-
-#  cp -r inputs "$PARENT_DIR/"
-#  cp -r system 0.orig Allrun.case "$PARENT_DIR/"
-#  cp constant/transportProperties "$CONSTANT_DIR"
-
-#  for file in transportProperties turbulenceProperties triSurface; do
-#    cd "$CONSTANT_DIR"
-#    ln -s "../../../constant/$file" "$file"
-#  done
-
-#  fileName=$(basename "${STL_FILES[$k]}")
-#  filePath="${STL_FILES[$k]}"
-
-#  eval foamDictionary inputs -entry stlNameFull \
-#    -set \'\"${filePath}\"\' -disableFunctionEntries
-#  foamDictionary inputs -entry stlName -set "$fileName" -disableFunctionEntries
-#  foamDictionary inputs -entry Ux -set "${U_PARAMS[$k]}" -disableFunctionEntries
-
-#  shape=$(foamDictionary -entry stlName inputs -value)
-#  printf '%s\n' "${shape%.*}" >shape
-#
-#  cd "$PARENT_DIR"
-#  ./Allrun.case
-
-#  OUTPUT_DIRS=($(foamListTimes -time '.*' -case .))
-#  OUTPUT_DIR="${OUTPUT_DIRS[-1]}"
-#
-#  cd ../sequencedVTU
-#  ln -s "$PARENT_DIR/base/0" "$OUTPUT_DIR"
-#  cd ../sequencedSTL
-#  ln -s "$CONSTANT_DIR/triSurface/$fileName" "${OUTPUT_DIR}.stl"
-#}
 
 run_case () {
   CASE_DIR="runs/$k"
